41a850a7becc4b2bc5361c791cc919eb47790d45
JeyKul <77121325+JeyKul@users.noreply.github.com>
2023-04-09 20:01:46 +0200

    a complete redesign

--- warn.js
+++
@@ -1,8 +0,0 @@
-const warningContainer = document.getElementById("warning-container");
-
-warningContainer.addEventListener("mousemove", (event) => {
-    const x = event.clientX - warningContainer.offsetLeft;
-    const y = event.clientY - warningContainer.offsetTop;
-    const deg = Math.atan2(y, x) * (180 / Math.PI);
-    warningContainer.style.transform = `rotate(${deg}deg)`;
-});
